6b56b51db2f3d36bae8f9e079b98387f8a459972
Poochly <743530@student.dusd.net>
2022-12-05 15:27:21 -0800

    fIXED ROBOT
    added robot left walked animation and fixed walking

--- RubysAdventureGallegosP7/Assets/Scripts/EnemyController.cs
+++ RubysAdventureGallegosP7/Assets/Scripts/EnemyController.cs
@@ -12,7 +12,6 @@ public class EnemyController : MonoBehaviour
     float timer;
     int direction = 1;
 
-    // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
@@ -29,14 +28,14 @@ public class EnemyController : MonoBehaviour
             timer = changeTime;
         }
     }
-    // Update is called once per frame
+
     void FixedUpdate()
     {
         Vector2 position = rigidbody2D.position;
 
         if (verticle)
         {
-            position.x = position.x + Time.deltaTime * speed;;
+            position.y = position.y + Time.deltaTime * speed * direction;;
         }
         else
         {
@@ -44,5 +43,14 @@ public class EnemyController : MonoBehaviour
         }
 
         rigidbody2D.MovePosition(position);
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
     }
 }
